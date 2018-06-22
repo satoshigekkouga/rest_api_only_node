92fe24d931def1181b3a9da1d253ddfea043fd83
Satoshi <gdg1893@gmail.com>
2018-06-22 09:45:27 +0530

    basic structure

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const {req_processing} = require('./lib/request_utils.js');
+const {lib} = require('./lib/data');
+
+const server = http.createServer(req_processing);
+
+server.listen(8000,() => {
+  console.log('listening at port 8000');
+});
+
+server.on('error',(err) => {
+  console.log(err);
+});
